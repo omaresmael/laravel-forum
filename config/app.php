8a348fc3f5b386d34ccde555d40cbd9030c24f4e
omaresmael <omaresmael@github.com>
2020-10-02 18:37:01 +0200

    linking database

--- config/app.php
+++ config/app.php
@@ -82,6 +82,8 @@ return [
 
     'locale' => 'en',
 
+    'log'=>'errorlog',
+
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
